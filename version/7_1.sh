41e6e830e0af5f7f5097194540b23cc76b3352de
mrinalam <mrinal.cena@gmail.com>
2017-02-07 14:26:03 +0000

    Updated PHP version

---
+++ version/7_1.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -e
+
+#Build PHP 7.1.1
+echo "============ Building PHP 7.1 =============="
+php-build -i development 7.1.1 $HOME/.phpenv/versions/7.1
+
+# Setting phpenv to 7.1.1
+echo "============ Setting phpenv to 7.1 ============"
+phpenv rehash
+phpenv global 7.1
+
+# Install phpunit
+echo "============ Installing PHPUnit ============="
+wget https://phar.phpunit.de/phpunit.phar
+chmod +x phpunit.phar
+mv phpunit.phar $HOME/.phpenv/versions/7.1/bin/phpunit
+
+# Install Composer
+echo "============ Installing Composer ============"
+curl -s http://getcomposer.org/installer | php
+chmod +x composer.phar
+mv composer.phar $HOME/.phpenv/versions/7.1/bin/composer
+
+#install pickle
+cd /tmp/pickle
+$HOME/.phpenv/versions/7.1/bin/composer install
+
+# Install php extensions
+echo "=========== Installing PHP extensions =============="
+printf '\n' | bin/pickle install memcache
+printf '\n' | bin/pickle install memcached
+printf '\n' | bin/pickle install mongo
+printf '\n' | bin/pickle install amqp-1.6.0
+printf '\n' | bin/pickle install zmq-beta
+printf '\n' | bin/pickle install redis
+
+cd /
+
+
