7c09b6640be0419dca1746c525c3f6bd3bcedac5
chetantarale <chetan_tarale@yahoo.com>
2017-03-28 14:26:43 +0530

    run wget and curl with right flags

--- version/7_0.sh
+++ version/7_0.sh
@@ -11,13 +11,13 @@ phpenv global 7.0
 
 # Install phpunit
 echo "============ Installing PHPUnit ============="
-wget https://phar.phpunit.de/phpunit-5.7.phar
+wget -nv https://phar.phpunit.de/phpunit-5.7.phar
 chmod +x phpunit-5.7.phar
 mv phpunit-5.7.phar $HOME/.phpenv/versions/7.0/bin/phpunit
 
 # Install Composer
 echo "============ Installing Composer ============"
-curl -s http://getcomposer.org/installer | php
+curl -sS http://getcomposer.org/installer | php
 chmod +x composer.phar
 mv composer.phar $HOME/.phpenv/versions/7.0/bin/composer
 
