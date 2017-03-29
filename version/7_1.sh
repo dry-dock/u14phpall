c77ce1844c5fc2200edebeee2df6148ae6095cb8
Ragesh Krishna <ragesh@rageshkrishna.com>
2017-03-29 15:51:34 +0530

    Fix all PHP7 extensions

--- version/7_1.sh
+++ version/7_1.sh
@@ -27,12 +27,14 @@ $HOME/.phpenv/versions/7.1/bin/composer install
 
 # Install php extensions
 echo "=========== Installing PHP extensions =============="
-printf '\n' | bin/pickle install memcache
 printf '\n' | bin/pickle install memcached
-printf '\n' | bin/pickle install mongo
-printf '\n' | bin/pickle install amqp-1.6.0
+printf '\n' | bin/pickle install amqp
 printf '\n' | bin/pickle install zmq-beta
 printf '\n' | bin/pickle install redis
+
+echo "--with-openssl-dir=yes" >> /tmp/pickle-mongodb-opts
+printf '\n' | bin/pickle install --with-configure-options=/tmp/pickle-mongodb-opts mongodb
+rm /tmp/pickle-mongodb-opts
 
 cd /
 
