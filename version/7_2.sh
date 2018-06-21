a5800731be9025a39ea72a259cded9903cf71e68
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-06-21 18:52:53 +0530

    Update 7_2.sh

--- version/7_2.sh
+++ version/7_2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-#Build PHP 7.2.5
-echo "============ Building PHP 7.2.5 =============="
-PHP_BUILD_CONFIGURE_OPTS="--with-bz2 --enable-intl" php-build -i development 7.2.5 $HOME/.phpenv/versions/7.2
+#Build PHP 7.2.6
+echo "============ Building PHP 7.2.6 =============="
+PHP_BUILD_CONFIGURE_OPTS="--with-bz2 --enable-intl" php-build -i development 7.2.6 $HOME/.phpenv/versions/7.2
 
-# Setting phpenv to 7.2.5
+# Setting phpenv to 7.2.6
 echo "============ Setting phpenv to 7.2 ============"
 phpenv rehash
 phpenv global 7.2
@@ -14,6 +14,23 @@ echo "============ Installing PHPUnit ============="
 wget -nv https://phar.phpunit.de/phpunit-7.0.3.phar
 chmod +x phpunit-7.0.3.phar
 mv phpunit-7.0.3.phar $HOME/.phpenv/versions/7.2/bin/phpunit
+
+#install pickle
+cd /tmp/pickle
+$HOME/.phpenv/versions/7.1/bin/composer install
+
+# Install php extensions
+echo "=========== Installing PHP extensions =============="
+printf '\n' | bin/pickle install memcached
+printf '\n' | bin/pickle install amqp
+printf '\n' | bin/pickle install zmq-beta
+printf '\n' | bin/pickle install redis
+
+echo "--with-openssl-dir=yes" >> /tmp/pickle-mongodb-opts
+printf '\n' | bin/pickle install --with-configure-options=/tmp/pickle-mongodb-opts mongodb
+rm /tmp/pickle-mongodb-opts
+
+cd /
 
 
 
