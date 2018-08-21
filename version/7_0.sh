822ede122d4f7bdb630c3843d4eb6e6089f61246
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-08-21 14:51:09 +0530

    installing ldap

--- version/7_0.sh
+++ version/7_0.sh
@@ -22,6 +22,7 @@ curl -sS http://getcomposer.org/installer | php
 chmod +x composer.phar
 mv composer.phar $HOME/.phpenv/versions/7.0/bin/composer
 
+sudo apt-get install php7.0-ldap
 #install pickle
 cd /tmp/pickle
 $HOME/.phpenv/versions/7.0/bin/composer install
